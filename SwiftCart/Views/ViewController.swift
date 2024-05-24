0e022a71cde6f065f46e5223e832cfcb5962820f
Marwaaboalata <113009464+Marwaaboalata@users.noreply.github.com>
2024-05-24 22:20:59 +0300

    Fetch Brands

--- SwiftCart/Views/ViewController.swift
+++ SwiftCart/Views/ViewController.swift
@@ -11,13 +11,19 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        BrandService.fetchBrands { res in
+            switch res {
+            case .success(_) : print("success")
+            case .failure(_):
+                print("error")
+            }
+        }
         
     }
     override func viewWillAppear(_ animated: Bool) {
-        let storyboard1 = UIStoryboard(name: "HomeAndCategories", bundle: nil)
-        let home = (storyboard1.instantiateViewController(withIdentifier: "HomeViewController") as? HomeViewController)!
-        navigationController?.pushViewController(home, animated: true)
+//        let storyboard1 = UIStoryboard(name: "HomeAndCategories", bundle: nil)
+//        let home = (storyboard1.instantiateViewController(withIdentifier: "HomeViewController") as? HomeViewController)!
+//        navigationController?.pushViewController(home, animated: true)
     }
    
 }
