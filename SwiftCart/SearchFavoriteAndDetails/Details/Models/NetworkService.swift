ecac13bb045092dfee25e97a231599e5435ac692
ramezHamdiSa'eed <ramez.hamdi4work@gmail.com>
2024-06-04 14:39:30 -0700

    products Details UI Development (demo)

---
+++ SwiftCart/SearchFavoriteAndDetails/Details/Models/NetworkService.swift
@@ -0,0 +1,39 @@
+//
+//  NetworkService.swift
+//  SwiftCart
+//
+//  Created by Ramez Hamdi Saeed on 04/06/2024.
+//
+
+import Foundation
+
+class NetworkService {
+    
+    var productDetailsResponse : ProductDetailsResponse
+    
+    func fetchProducts() -> Observable<[ProductTemp]> {
+            AppCommon.networkingManager.networkingRequest(
+                path: "/products.json",
+                queryItems: nil,
+                method: .GET,
+                requestBody: nil,
+                networkResponse: { (result: Result<ProductResponse, NetworkError>) in
+                    switch result {
+                    case .success(let productResponse):
+                        if let products = productResponse.products {
+                            print(products)
+                        } else {
+                            print("empty returned products")
+                        }
+                    case .failure(let error):
+                        print("network fetch products error: \(error.localizedDescription)")
+                    }
+                }
+            )
+        
+        return Observable.just([
+                   ProductTemp(id: "1", name: "Back Bag", price: 0.0, isFavorite: false,image: "https://cdn.shopify.com/s/files/1/0702/9630/5915/files/8072c8b5718306d4be25aac21836ce16.jpg?v=1716706068"),
+                   ProductTemp(id: "2", name: "Back Bag | Addidas", price: 2.0, isFavorite: true,image: "https://cdn.shopify.com/s/files/1/0702/9630/5915/files/8072c8b5718306d4be25aac21836ce16.jpg?v=1716706068")
+               ])
+    }
+}
---
+++ SwiftCart/SearchFavoriteAndDetails/Details/Models/ProductDetailsResponse.swift
@@ -0,0 +1,8 @@
+//
+//  ProductDetailsResponse.swift
+//  SwiftCart
+//
+//  Created by Ramez Hamdi Saeed on 04/06/2024.
+//
+
+import Foundation
