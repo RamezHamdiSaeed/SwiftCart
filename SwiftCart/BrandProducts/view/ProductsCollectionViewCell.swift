14ecffe243d4bb9a3d84c9df4004a64a4fdc946d
Marwaaboalata <113009464+Marwaaboalata@users.noreply.github.com>
2024-06-06 20:06:21 +0300

    isHidden for love images in cells

--- SwiftCart/BrandProducts/view/ProductsCollectionViewCell.swift
+++ SwiftCart/BrandProducts/view/ProductsCollectionViewCell.swift
@@ -9,14 +9,24 @@ import UIKit
 
 class ProductsCollectionViewCell: UICollectionViewCell {
     var product : Product?
+    var isClicked = false
     @IBOutlet weak var productImage: UIImageView!
     @IBOutlet weak var productName: UILabel!
     @IBOutlet weak var productPrice: UILabel!
     
   
-  
+    @IBOutlet weak var fillLove: UIImageView!
+    
+    @IBOutlet weak var emptyLove: UIImageView!
     
     @IBAction func addToFAV(_ sender: Any) {
+        if !isClicked{
+            emptyLove.isHidden = true
+            fillLove.isHidden = false
+        }else{
+            emptyLove.isHidden = false
+            fillLove.isHidden = true
+        }
         print("Faaaaaaaaaaav")
         print(product?.title)
     }
