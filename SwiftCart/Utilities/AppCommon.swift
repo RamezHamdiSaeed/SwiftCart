c3a18adbb7e84f5510c8f33b63cab435d33bef77
ramezHamdiSa'eed <ramez.hamdi4work@gmail.com>
2024-06-10 06:30:33 -0700

    fix authenticated user id

--- SwiftCart/Utilities/AppCommon.swift
+++ SwiftCart/Utilities/AppCommon.swift
@@ -9,7 +9,6 @@ import Foundation
 
 
 class AppCommon{
-    static var user = User()
     static var networkingManager = NetworkingManager()
     static var feedbackManager = FeedbackManager()
     static var locationManager = LocationManager()
