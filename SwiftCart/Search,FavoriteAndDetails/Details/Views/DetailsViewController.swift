9a55eea40f0c2522dee0b087cec9b13a79173227
ramezHamdiSa'eed <ramez.hamdi4work@gmail.com>
2024-06-13 06:50:37 -0700

    pass selected product quantity

--- SwiftCart/Search,FavoriteAndDetails/Details/Views/DetailsViewController.swift
+++ SwiftCart/Search,FavoriteAndDetails/Details/Views/DetailsViewController.swift
@@ -108,22 +108,13 @@ class DetailsViewController: UIViewController {
         }
     }
     @IBAction func addToCartBtn(_ sender: Any) {
-        guard let selectedVariant = detailsViewModel.selectedProductVarient else {
-                print("No variant selected")
-                return
-            }
-            
-            let variantID = selectedVariant.id
-            
-            
-            print("The selected product variant added to cart: \(variantID)")
-            
+        
         guard let selectedVariant = detailsViewModel.selectedProductVarient else {
                     print("No variant selected")
                     return
                 }
                 
-        let lineItem = LineItemRequest(variantID: selectedVariant.id ?? 0, quantity: 1, imageUrl: productimgUrl)
+        let lineItem = LineItemRequest(variantID: selectedVariant.id ?? 0, quantity: selectedVariant.inventoryQuantity!, imageUrl: productimgUrl)
                 cartViewModel.addToCart(customerId: customerID ?? 0, lineItem: lineItem)        
 
         print("the selected product varient added to cart : \(detailsViewModel.selectedProductVarient!)")
