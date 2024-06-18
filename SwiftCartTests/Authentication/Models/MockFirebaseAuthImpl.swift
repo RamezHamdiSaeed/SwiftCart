f42eb2b458634b94b1a0d0ab278edf51d58e8b12
ramezHamdiSa'eed <ramez.hamdi4work@gmail.com>
2024-06-18 10:58:43 -0700

    test auth model (firebase)

---
+++ SwiftCartTests/Authentication/Models/MockFirebaseAuthImpl.swift
@@ -0,0 +1,87 @@
+//
+//  FirebaseAuthImpl.swift
+//  SwiftCartTests
+//
+//  Created by Ramez Hamdi Saeed on 18/06/2024.
+//
+
+import Foundation
+import Firebase
+import XCTest
+@testable import SwiftCart
+
+class MockFirebaseAuthImpl : FirebaseAuth{
+   
+    init() {
+        if FirebaseApp.app() == nil {
+            FirebaseApp.configure()
+        }
+    }
+    
+    func deleteUserAccount(completion: @escaping (Error?) -> Void) {
+        guard let user = Auth.auth().currentUser else {
+            completion(nil)
+            return
+        }
+        
+        user.delete { error in
+            completion(error)
+        }
+    }
+    
+    func signUp(email: String, password: String) {
+        Auth.auth().createUser(withEmail: email, password: password) { result, error in
+            if let error = error {
+                print("Registration failed with error: \(error.localizedDescription)")
+                // Handle error appropriately (e.g., show an alert)
+            } else {
+                print("User registered successfully")
+                // Handle success (e.g., navigate to the main screen)
+            }
+        }
+    }
+    
+    func logIn(email: String, password: String, whenSuccess: @escaping () -> ()) {
+        Auth.auth().signIn(withEmail: email, password: password) { result, error in
+            if let error = error {
+                print("Login failed with error: \(error.localizedDescription)")
+                // Handle error appropriately (e.g., show an alert)
+            } else {
+                print("User logged in successfully")
+                whenSuccess()
+            }
+        }
+    }
+    
+    func signOut(whenSuccess: @escaping () -> ()) {
+        do {
+            try Auth.auth().signOut()
+            print("User signed out successfully")
+            whenSuccess()
+        } catch let error {
+            print("Sign out failed with error: \(error.localizedDescription)")
+            // Handle error appropriately (e.g., show an alert)
+        }
+    }
+    func deleteUserByEmail(email: String, password: String) {
+        Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+            if let error = error {
+                print("Error signing in: \(error.localizedDescription)")
+                return
+            }
+
+            guard let user = authResult?.user else {
+                print("No user found")
+                return
+            }
+
+            user.delete { error in
+                if let error = error {
+                    print("Error deleting user: \(error.localizedDescription)")
+                } else {
+                    print("User deleted successfully")
+                }
+            }
+        }
+    }
+}
--- SwiftCartTests/SwiftCartTests.swift
+++ SwiftCartTests/SwiftCartTests.swift
@@ -9,18 +9,21 @@ import XCTest
 @testable import SwiftCart
 
 final class SwiftCartTests: XCTestCase {
-    
+ 
     var mockNetworkServices : MockNetworkServices!
 
     override func setUpWithError() throws {
         mockNetworkServices = MockNetworkServices(shouldReturnError: false)
         // Put setup code here. This method is called before the invocation of each test method in the class.
+
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
 
+    }
+    
+    
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
@@ -28,6 +31,8 @@ final class SwiftCartTests: XCTestCase {
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
     }
+    
+    
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
@@ -100,7 +105,7 @@ final class SwiftCartTests: XCTestCase {
                 expect.fulfill()
             }
         }
-        waitForExpectations(timeout: 5)
+        waitForExpectations(timeout: 10)
     }
     
     func testFetchMockBrands() {
