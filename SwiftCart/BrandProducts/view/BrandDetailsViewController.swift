e530a27e30ad48c5a532ab08276938feb5b96308
Marwaaboalata <113009464+Marwaaboalata@users.noreply.github.com>
2024-06-20 02:23:27 +0300

    add Empty Image in product details

--- SwiftCart/BrandProducts/view/BrandDetailsViewController.swift
+++ SwiftCart/BrandProducts/view/BrandDetailsViewController.swift
@@ -130,6 +130,166 @@
 //    }
 //}
 //
+
+
+//import UIKit
+//import SDWebImage
+//
+//class BrandDetailViewController: UIViewController, UISearchBarDelegate {
+//    private var favViewModel: SearchFavoriteProductsViewModel!
+//    private var filteredProductsArray: [Product] = []
+//    private var isSearching = false
+//
+//
+//    var price = 0.0
+//    @IBOutlet weak var brandProductsCollectionView: UICollectionView!
+//
+//    @IBOutlet weak var emptyImage: UIImageView!
+//    var collectionIdStr: Int = 0
+//    var collectionTitle = ""
+//    var productViewModel = ProductsViewModel(networkService: NetworkServicesImpl())
+//    var productsArray: [Product] = []
+//    var rate: Double!
+//    let userCurrency = CurrencyImp.getCurrencyFromUserDefaults().uppercased()
+//
+//    let searchBar = UISearchBar()
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        searchBar.delegate = self
+//        searchBar.placeholder = "Search in \(collectionTitle)"
+//        self.navigationItem.titleView = searchBar
+//
+//        productViewModel.rateClosure = {
+//            [weak self] rate in
+//            DispatchQueue.main.async {
+//                self?.rate = rate
+//            }
+//        }
+//        productViewModel.getRate()
+//        //setHeader(view: self, title: collectionTitle)
+//        let productNibFile = UINib(nibName: "SingleProductCollectionViewCell", bundle: nil)
+//        brandProductsCollectionView.register(productNibFile, forCellWithReuseIdentifier: "cell")
+//
+//        CollectionViewDesign.collectionView(colView: brandProductsCollectionView)
+//        brandProductsCollectionView.dataSource = self
+//        brandProductsCollectionView.delegate = self
+//
+//        productViewModel.productsClosure = { [weak self] res in
+//            DispatchQueue.main.async {
+//                self?.productsArray = res
+//                self?.brandProductsCollectionView.reloadData()
+//                self?.checkEmpty()
+//            }
+//        }
+//
+//        productViewModel.getProducts(collectionId: collectionIdStr)
+//        checkEmpty()
+//
+//    }
+//
+//    override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(animated)
+//
+//        productViewModel.productsClosure = { [weak self] res in
+//            DispatchQueue.main.async {
+//                self?.productsArray = res
+//                self?.brandProductsCollectionView.reloadData()
+//            }
+//        }
+//
+//        productViewModel.getProducts(collectionId: collectionIdStr)
+//
+//        favViewModel = {
+//            let searchFavoriteProductsVC = SearchFavoriteProductsViewModel(networkService: SearchNetworkService(networkingManager: NetworkingManagerImpl()))
+//            searchFavoriteProductsVC.getFavoriteProductsDB()
+//            return searchFavoriteProductsVC
+//        }()
+//        checkEmpty()
+//
+//    }
+//
+//    // UISearchBarDelegate method
+//    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+//        if searchText.isEmpty {
+//            isSearching = false
+//            brandProductsCollectionView.reloadData()
+//        } else {
+//            isSearching = true
+//            filteredProductsArray = productsArray.filter { product in
+//                return product.title.lowercased().contains(searchText.lowercased())
+//            }
+//            brandProductsCollectionView.reloadData()
+//        }
+//    }
+//}
+//
+//extension BrandDetailViewController: UICollectionViewDataSource {
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return isSearching ? filteredProductsArray.count : productsArray.count
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? SingleProductCollectionViewCell else {
+//            return UICollectionViewCell()
+//        }
+//        cell.whenRemoving = {
+//            AppCommon.feedbackManager.showCancelableAlert(alertTitle: "Prompt", alertMessage: "Do you want to remove from Favs", alertStyle: .alert, view: self){
+//                cell.okRemovingCellBtn()
+//            }
+//        }
+//        cell.guestClosure = {
+//            AppCommon.feedbackManager.showAlert(alertTitle: "Prompt", alertMessage: "You need to Log In", alertStyle: .alert, view: self)
+//        }
+//
+//        let product = isSearching ? filteredProductsArray[indexPath.item] : productsArray[indexPath.item]
+//        cell.productName.text = product.title
+//        if let imageUrl = URL(string: product.image.src) {
+//            cell.productImage.sd_setImage(with: imageUrl, placeholderImage: UIImage(named: "processing"))
+//        } else {
+//            cell.productImage.image = UIImage(named: "processing")
+//        }
+//
+//        let convertedPrice = convertPrice(price: product.variants[0].price, rate: self.rate)
+//        cell.productPrice.text = "\(String(format: "%.2f", convertedPrice)) \(userCurrency)"
+//        CollectionViewDesign.collectionViewCell(cell: cell)
+//
+//        var productTemp = ProductTemp(id: product.id, name: product.title, price: Double(product.variants[0].price)!, isFavorite: false, image: product.image.src)
+//        productTemp.isFavorite = favViewModel.isProductFavorite(product: productTemp)
+//
+//        cell.product = productTemp
+//        cell.toggleFavBtn()
+//
+//        return cell
+//    }
+//
+//    func checkEmpty(){
+//        if productsArray.isEmpty {
+//            emptyImage.isHidden = !filteredProductsArray.isEmpty
+//
+//
+//        }else {
+//            emptyImage.isHidden = !productsArray.isEmpty
+//        }
+//    }
+//}
+//
+//extension BrandDetailViewController: UICollectionViewDelegate {
+//    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+//        let product = isSearching ? filteredProductsArray[indexPath.item] : productsArray[indexPath.item]
+//        let productsSearchDetailsAndFav = UIStoryboard(name: "ProductsSearchDetailsAndFav", bundle: nil)
+//        let detailsViewController = (productsSearchDetailsAndFav.instantiateViewController(withIdentifier: "DetailsViewController")) as! DetailsViewController
+//        detailsViewController.productID = String(product.id)
+//        self.navigationController?.pushViewController(detailsViewController, animated: true)
+//    }
+//}
+//
+//extension BrandDetailViewController: UICollectionViewDelegateFlowLayout {
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        return CGSize(width: 150, height: 254)
+//    }
+//}
 import UIKit
 import SDWebImage
 
@@ -140,12 +300,13 @@ class BrandDetailViewController: UIViewController, UISearchBarDelegate {
 
     var price = 0.0
     @IBOutlet weak var brandProductsCollectionView: UICollectionView!
-    
+   
     var collectionIdStr: Int = 0
     var collectionTitle = ""
     var productViewModel = ProductsViewModel(networkService: NetworkServicesImpl())
     var productsArray: [Product] = []
     var rate: Double!
+    @IBOutlet weak var emptyImage: UIImageView!
     let userCurrency = CurrencyImp.getCurrencyFromUserDefaults().uppercased()
     
     let searchBar = UISearchBar()
@@ -157,14 +318,13 @@ class BrandDetailViewController: UIViewController, UISearchBarDelegate {
         searchBar.placeholder = "Search in \(collectionTitle)"
         self.navigationItem.titleView = searchBar
         
-        productViewModel.rateClosure = {
-            [weak self] rate in
-                DispatchQueue.main.async {
-                    self?.rate = rate
-                }
+        productViewModel.rateClosure = { [weak self] rate in
+            DispatchQueue.main.async {
+                self?.rate = rate
+            }
         }
         productViewModel.getRate()
-        //setHeader(view: self, title: collectionTitle)
+        
         let productNibFile = UINib(nibName: "SingleProductCollectionViewCell", bundle: nil)
         brandProductsCollectionView.register(productNibFile, forCellWithReuseIdentifier: "cell")
         
@@ -176,10 +336,12 @@ class BrandDetailViewController: UIViewController, UISearchBarDelegate {
             DispatchQueue.main.async {
                 self?.productsArray = res
                 self?.brandProductsCollectionView.reloadData()
+                self?.checkEmpty()
             }
         }
         
         productViewModel.getProducts(collectionId: collectionIdStr)
+        checkEmpty()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -189,6 +351,7 @@ class BrandDetailViewController: UIViewController, UISearchBarDelegate {
             DispatchQueue.main.async {
                 self?.productsArray = res
                 self?.brandProductsCollectionView.reloadData()
+                self?.checkEmpty()
             }
         }
         
@@ -199,6 +362,8 @@ class BrandDetailViewController: UIViewController, UISearchBarDelegate {
             searchFavoriteProductsVC.getFavoriteProductsDB()
             return searchFavoriteProductsVC
         }()
+        
+        checkEmpty()
     }
     
     // UISearchBarDelegate method
@@ -213,6 +378,15 @@ class BrandDetailViewController: UIViewController, UISearchBarDelegate {
             }
             brandProductsCollectionView.reloadData()
         }
+        checkEmpty()
+    }
+    
+    func checkEmpty() {
+        if isSearching {
+            emptyImage.isHidden = !filteredProductsArray.isEmpty
+        } else {
+            emptyImage.isHidden = !productsArray.isEmpty
+        }
     }
 }
 
@@ -226,7 +400,7 @@ extension BrandDetailViewController: UICollectionViewDataSource {
             return UICollectionViewCell()
         }
         cell.whenRemoving = {
-            AppCommon.feedbackManager.showCancelableAlert(alertTitle: "Prompt", alertMessage: "Do you want to remove from Favs", alertStyle: .alert, view: self){
+            AppCommon.feedbackManager.showCancelableAlert(alertTitle: "Prompt", alertMessage: "Do you want to remove from Favs", alertStyle: .alert, view: self) {
                 cell.okRemovingCellBtn()
             }
         }
@@ -260,7 +434,7 @@ extension BrandDetailViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let product = isSearching ? filteredProductsArray[indexPath.item] : productsArray[indexPath.item]
         let productsSearchDetailsAndFav = UIStoryboard(name: "ProductsSearchDetailsAndFav", bundle: nil)
-        let detailsViewController = (productsSearchDetailsAndFav.instantiateViewController(withIdentifier: "DetailsViewController")) as! DetailsViewController
+        let detailsViewController = productsSearchDetailsAndFav.instantiateViewController(withIdentifier: "DetailsViewController") as! DetailsViewController
         detailsViewController.productID = String(product.id)
         self.navigationController?.pushViewController(detailsViewController, animated: true)
     }
