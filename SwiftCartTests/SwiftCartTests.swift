5b91e9f775dca98ce20c0188e9424a1fb1d5bc50
Marwaaboalata <113009464+Marwaaboalata@users.noreply.github.com>
2024-06-20 01:35:57 +0300

    fix test

--- SwiftCartTests/SwiftCartTests.swift
+++ SwiftCartTests/SwiftCartTests.swift
@@ -493,3 +493,127 @@ final class OrdersViewModelTests: XCTestCase {
         waitForExpectations(timeout: 1, handler: nil)
     }
 }
+
+final class SwiftCartTests: XCTestCase {
+    var mockNetworkServices : MockNetworkServices!
+    var networkServices : NetworkServices!
+   
+    
+    override func setUpWithError() throws {
+        mockNetworkServices = MockNetworkServices(shouldReturnError: false)
+        networkServices = NetworkServicesImpl()
+        
+    }
+    
+    override func tearDownWithError() throws {
+        mockNetworkServices = nil
+        networkServices = nil
+        
+    }
+   
+    func testFetchBrands() {
+        let expect = expectation(description: "test Fetch Brands from network")
+        
+        networkServices.fetchBrands { resRes in
+            switch resRes {
+            case .success(data: let data):
+                XCTAssertNotNil(data)
+                expect.fulfill()
+            case .failure(error: let error):
+                XCTFail("Failed")
+                expect.fulfill()
+            }
+        }
+        waitForExpectations(timeout: 5)
+    }
+    func testFetchProductsForSubCategory() {
+        let expect = expectation(description: "Fetch Products for SubCategory from network")
+        
+        networkServices.fetchProductsForSubCategory(productType: "Shoes") { result in
+            switch result {
+            case .success(let data):
+                XCTAssertNotNil(data)
+                expect.fulfill()
+            case .failure(let error):
+                XCTFail("Failed with error: \(error)")
+                expect.fulfill()
+            }
+        }
+        waitForExpectations(timeout: 5)
+    }
+    func testFetchOrders() {
+        let expect = expectation(description: "Fetch Orders from network")
+        
+        networkServices.fetchOrders(customerId: "7495574716667") { result in
+            switch result {
+            case .success(let data):
+                XCTAssertNotNil(data)
+                XCTAssertTrue(data.orders?.count != 0)
+                expect.fulfill()
+            case .failure(let error):
+                XCTFail("Failed with error: \(error)")
+                expect.fulfill()
+            }
+        }
+        waitForExpectations(timeout: 5)
+    }
+    func testFetchingProducts() {
+        let expect = expectation(description: "Fetch Products from network")
+        networkServices.fetchProducts(collectionId: "422258540795"){
+            result in
+            switch result {
+            case .success(let data):
+                XCTAssertNotNil(data)
+                XCTAssertTrue(data.products.count != 0)
+                
+                expect.fulfill()
+            case .failure(let error):
+                XCTFail("Failed with error")
+                expect.fulfill()
+            }
+        }
+        waitForExpectations(timeout: 5)
+        
+    }
+    func testFetchMockBrands() {
+        //  let expect = expectation(description: "Fetch Mock Brands from mock service")
+        mockNetworkServices.fetchBrands { resRes in
+            switch resRes {
+            case .success(let data):
+                XCTAssertNotNil(data)
+                XCTAssertEqual(data.smartCollections.count, 1)
+                XCTAssertEqual(data.smartCollections.first?.title, "ADIDAS")
+                //         expect.fulfill()
+            case .failure(let error):
+                XCTFail("Failed with error: \(error)")
+                //         expect.fulfill()
+            }
+        }
+        // waitForExpectations(timeout: 5)
+    }
+    func testFetchMockProdcts() {
+        
+        mockNetworkServices.fetchProducts(collectionId: "422258540795" ){ resRes in
+            switch resRes {
+            case .success(let data):
+                XCTAssertNotNil(data)
+                XCTAssertNotNil(data.products.count != 0)
+                XCTAssertEqual(data.products.first?.title, "ADIDAS | CLASSIC BACKPACK")
+            case .failure(let error):
+                XCTFail("Failed with error: \(error)")
+            }
+        }
+    }
+    func testFetchMockOrders() {
+        mockNetworkServices.fetchOrders(customerId: "7495574716667" ){ resRes in
+            switch resRes {
+            case .success(let data):
+                XCTAssertNotNil(data)
+                XCTAssertNotNil(data.orders?.count != 0)
+            case .failure(let error):
+                XCTFail("Failed with error: \(error)")
+            }
+        }
+    }
+    
+}
